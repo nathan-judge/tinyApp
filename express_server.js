1107162f34654ba92113a36e47ee52243a97ee3b
nathan-judge <nathanjudge27@gmail.com>
2021-09-27 21:38:42 +0000

    urls and shorten url shows

--- express_server.js
+++ express_server.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080; // default port is 8080
+
 app.set("view engine", "ejs");
+
+// using this Object to keep track of all the URLs and their shortened forms
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
+    "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
     res.send("Hello!");
 });
-app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
+
+app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render('urls_index', templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
